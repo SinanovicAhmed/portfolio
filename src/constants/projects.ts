75fae99df52cf1b5b48450e5d7922fecf4579dda
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2024-08-09 22:38:00 +0200

    Update: refined project video links added

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -6,7 +6,7 @@ export const projects = [
       "A user-friendly platform for browsing job listings with easy pagination and detailed job descriptions. It includes a form for posting new jobs and an admin dashboard for approving or deleting job postings, ensuring efficient management and a seamless job searching experience.",
     technologies: ["Next.js", "TypeScript", "TailwindCSS", "PostgreSQL", "Zod", "Prisma ORM", "Clerk"],
     gitLink: "https://github.com/SinanovicAhmed/nextjs_form",
-    videoLink: "https://www.youtube.com/watch?v=1d2eouA_nCw",
+    videoLink: "https://www.youtube.com/watch?v=T7056qGeZxk",
   },
   {
     name: "Whiteboard Collaboration",
@@ -15,7 +15,7 @@ export const projects = [
       "A real-time collaboration app that enables users to create or join rooms for shared drawing. It features Google login, room creation, and the ability to join active sessions. Users can collaboratively update the canvas in real-time, download the canvas as a image, and engage in room-specific chat.",
     technologies: ["React.js", "JavaScript", "Socket.io", "TailwindCSS", "GoogleAuth"],
     gitLink: "https://github.com/SinanovicAhmed/colabarative-whiteboard",
-    videoLink: "https://www.youtube.com/watch?v=io3d6Yro364",
+    videoLink: "https://www.youtube.com/watch?v=Wt4OMk8ExGI",
   },
 
   {
@@ -25,7 +25,7 @@ export const projects = [
       "A landing page for a car repair workshop designed to provide essential information in an engaging format. It includes distinct sections such as a Hero section, About section, Services section, and Contact section. Enhanced with AOS animations, the website offers an engaging user experience that captures visitors attention and encourages interaction.",
     technologies: ["Next.js", "JavaScript", "TailwindCSS", "AOS"],
     gitLink: "https://github.com/SinanovicAhmed/AutoRepairShop-LandingPage",
-    videoLink: "https://www.youtube.com/watch?v=BrzgPVRdIko",
+    videoLink: "https://www.youtube.com/watch?v=3dCl2pVwK2M",
   },
 
   {
@@ -35,7 +35,7 @@ export const projects = [
       "A web application designed to visualize sorting and searching algorithms. It provides detailed explanations, step-by-step breakdowns, and analyses of time and space complexities for each selected algorithm, making it an educational tool for understanding algorithmic concepts.",
     technologies: ["React.js", "JavaScript", "TailwindCSS"],
     gitLink: "https://github.com/SinanovicAhmed/algo-visualizer",
-    videoLink: "https://www.youtube.com/watch?v=FeN6os37UPE",
+    videoLink: "https://www.youtube.com/watch?v=HuInHpU7nA0",
     liveLink: "https://algo-visualizer-mu.vercel.app/",
   },
 
